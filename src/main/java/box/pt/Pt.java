f124799455a3115065401038d89ec0e21cd3b5ce
tawso <eskendir183@gmail.com>
2025-11-07 16:27:34 +0500

    pt 1

---
+++ src/main/java/box/pt/Pt.java
@@ -0,0 +1,10 @@
+package box.pt;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Pt implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/box/pt/client/PtClient.java
@@ -0,0 +1,10 @@
+package box.pt.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PtClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
